7820a3c49ee5a76d6cf47421e095a499736c8e46
Meghan Ferrall-Fairbanks <meghan.ferrall.fairbanks@gmail.com>
2020-11-12 08:21:20 -0500

    updated documenter

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,39 @@
-using OncoDiversity
 using Documenter
+using Literate
+
+const literate_dir = joinpath(@__DIR__, "literate")
+const generated_dir = joinpath(@__DIR__, "src", "generated")
+
+@info "Loading OncoDiversity.jl"
+using OncoDiversity
+
+# XXX: Work around old LaTeX distribution in GitHub CI.
+@info "Building Literate.jl docs"
+if haskey(ENV, "GITHUB_ACTIONS")
+    import TikzPictures
+    TikzPictures.standaloneWorkaround(true)
+  end
+
+# Set Literate.jl config if not being compiled on recognized service.
+config = Dict{String,String}()
+if !(haskey(ENV, "GITHUB_ACTIONS") || haskey(ENV, "GITLAB_CI"))
+  config["nbviewer_root_url"] = "https://nbviewer.jupyter.org/github/mcfefa/OncoDiversity.jl/blob/gh-pages/dev"
+  config["repo_root_url"] = "https://github.com/mcfefa/OncoDiversity.jl/blob/master/docs"
+end
+
+# for (root, dirs, files) in walkdir(literate_dir)
+#   out_dir = joinpath(generated_dir, relpath(root, literate_dir))
+#   for file in files
+#     if last(splitext(file)) == ".jl"
+#       Literate.markdown(joinpath(root, file), out_dir;
+#         config=config, documenter=true, credit=false)
+#       Literate.notebook(joinpath(root, file), out_dir;
+#         execute=true, documenter=true, credit=false)
+#     end
+#   end
+# end
+
+@info "Building Documenter.jl docs"
 
 makedocs(;
     modules=[OncoDiversity],
@@ -16,6 +50,10 @@ makedocs(;
     ],
 )
 
-deploydocs(;
-    repo="github.com/mcfefa/OncoDiversity.jl",
+
+@info "Deploying docs"
+deploydocs(
+  target = "build",
+  repo   = "github.com/mcfefa/OncoDiversity.jl.git",
+  branch = "gh-pages"
 )
